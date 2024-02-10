ea98d703b13c90ea36932ec5e4f0a451df9a9acb
hydaline-code <djougancharlen@gmail.com>
2024-02-10 08:58:16 +0100

    Add expense page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :password_confirmation, :current_password)
     end
   end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "categories#index"
+  root "splash#index"
 
 
-  
+  delete 'logout', to: 'devise/sessions#destroy', as: :logout
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
