2910d0cbea2aca2250ab6bf28b0ed21e78b8cff3
hydaline-code <djougancharlen@gmail.com>
2024-02-17 17:19:21 +0100

    remove commented code

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
- 
+  let(:category) { Category.create(name: "Test Category", icon: "https://example.com/icon.png") }
+
+  it "is valid with valid attributes" do
+    expense = Expense.new(name: "Test Expense", amount: 100, category: category)
+    expect(expense).to be_valid
+  end
+
+  it "is not valid without a name" do
+    expense = Expense.new(amount: 100, category: category)
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid without an amount" do
+    expense = Expense.new(name: "Test Expense", category: category)
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid if amount is not a number" do
+    expense = Expense.new(name: "Test Expense", amount: "invalid", category: category)
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid if amount is less than 0" do
+    expense = Expense.new(name: "Test Expense", amount: -100, category: category)
+    expect(expense).not_to be_valid
+  end
+
+  it "is not valid if amount is greater than 10,000" do
+    expense = Expense.new(name: "Test Expense", amount: 20_000, category: category)
+    expect(expense).not_to be_valid
+  end
+
+  it "belongs to a category" do
+    association = Expense.reflect_on_association(:category)
+    expect(association.macro).to eq(:belongs_to)
+  end
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe User, type: :model do
-#   describe 'User model' do
-#     before(:each) do
-#       @user = User.new(name: 'chacha', email: 'charlenei@gmail.com', password: 'password')
-#     end
-
-#     before { @user.save }
-
-#     it 'name must not be blank' do
-#       @user.name = nil
-#       expect(@user).to_not be_valid
-#     end
-
-#     it 'email must not be blank' do
-#       @user.email = ''
-#       expect(@user).to_not be_valid
-#     end
-
-#     it 'password must not be blank' do
-#       @user.password = ''
-#       expect(@user).to_not be_valid
-#     end
-#   end
-# end
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
