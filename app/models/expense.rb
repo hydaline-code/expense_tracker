db7767087a40cb39d8ebf848a97e929f28ea9f98
hydaline-code <djougancharlen@gmail.com>
2024-02-17 14:25:37 +0100

    Implement the error message

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,17 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true, length: { maximum: 250 }
-  validates :amount, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
-
-  private
-
-  def validate_amount_format
-    if amount.present? && (amount.to_s =~ /\A[+-]?\d+(\.\d+)?\z/).nil?
-      errors.add(:amount, "must be a valid number")
-    end
-
-    if amount.present? && amount.to_f > 1_000_000
-      errors.add(:amount, "cannot exceed 1,000,000")
-    end
-  end
+  validates :amount, presence: true, numericality: { only_decimal: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10_000, message: "must be less than or equal to 10,000" }
+  
 end
